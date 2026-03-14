4e56d205dcda8c48d0e3b47e19314214f68060ba
shatakshi-1404 <shatakshiravaneshwarprasad@gmail.com>
2026-03-14 14:39:08 +0530

    Improved image performance with lazy loading and added hover animation

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,12 +1,15 @@
 export default function PhotoCard({ photo, isFav, toggleFav }) {
   return (
-    <div className="bg-white shadow rounded p-2">
+    <div className="bg-white shadow rounded p-2 hover:scale-105 transition-transform duration-200">
+
 
       <img
-        src={photo.download_url}
-        alt={photo.author}
-        className="w-full h-48 object-cover rounded"
-      />
+  src={photo.download_url}
+  alt={photo.author}
+  loading="lazy"
+  className="w-full h-48 object-cover rounded"
+/>
+
 
       <div className="flex justify-between items-center mt-2">
 
